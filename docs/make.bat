a1c46f07ec19d521fd1b81ae38f26d577d386787
Caian R. Ertl <hi@caian.org>
2017-12-26 08:26:16 -0200

    docs: reinitialize Sphinx
    Undo the directory differentiation for source and builds.

--- docs/make.bat
+++ docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
-set BUILDDIR=build
-set SPHINXPROJ=cdots
+set SOURCEDIR=.
+set BUILDDIR=_build
+set SPHINXPROJ=dots
 
 if "%1" == "" goto help
 
