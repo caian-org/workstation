6ed13752188feaa0551316eff5178ae2d1ad4bb5
Caian R. Ertl <hi@caian.org>
2019-05-08 05:34:33 -0300

    feat: become root when installing packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,31 @@
 
 set -e
 
-pacman -Syyu --noconfirm
-pacman -S --noconfirm ansible
+# updates everything
+sudo pacman -Syyu --noconfirm
 
-ansible-playbook -i ansible/hosts ansible/workstation.yml
+# installs required packages
+sudo pacman -S --noconfirm ansible git wget pwgen
+
+# lets not polute the current directory
+tmp_dir=$(pwgen -cns 16 1)
+cd /tmp
+mkdir "${tmp_dir}"
+cd "${tmp_dir}"
+
+# downloads last release
+last_tag=$(git ls-remote --tags https://github.com/caian-org/workstation | awk -F '/' '{print $3}')
+wget "https://github.com/caian-org/workstation/archive/${last_tag}.tar.gz"
+
+# installs yay (aur helper)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+
+# extracts the release tarball
+tar xzf "${last_tag}.tar.gz"
+
+# runs the ansible playbook
+cd workstation-*
+ansible-playbook --ask-become-pass -i ansible/hosts ansible/workstation.yml
