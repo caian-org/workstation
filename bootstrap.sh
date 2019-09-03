034799ab158e0fca6b9ed66fc73ade20e4727d1e
Caian R. Ertl <hi@caian.org>
2019-09-02 23:38:52 -0300

    feat: include boostrap instructions to manjaro (linux) and mac os

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,32 +2,58 @@
 
 set -e
 
-# updates everything
-sudo pacman -Syyu --noconfirm
 
-# installs required packages
-sudo pacman -S --noconfirm ansible git wget pwgen
+preinstall_macos() {
+    # installs homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # installs required packages
+    brew install ansible git wget
+}
+
+preinstall_manjaro() {
+    # updates everything
+    sudo pacman -Syyu --noconfirm
+
+    # installs required packages
+    sudo pacman -S --noconfirm ansible git wget
+
+    # installs yay (aur helper)
+    git clone https://aur.archlinux.org/yay.git
+    (
+        cd yay
+        makepkg -si --noconfirm
+    )
+}
+
+run_playbook() {
+    # downloads last release
+    last_tag=$(git ls-remote --tags https://github.com/caian-org/workstation | awk -F '/' '{print $3}' | tail -n 1)
+    wget "https://github.com/caian-org/workstation/archive/${last_tag}.tar.gz"
+
+    # extracts the release tarball
+    tar xzf "${last_tag}.tar.gz"
+
+    # runs the ansible playbook
+    cd workstation-*
+    make run "os=$1"
+}
+
 
 # lets not polute the current directory
-tmp_dir=$(pwgen -cns 16 1)
-cd /tmp
-mkdir "${tmp_dir}"
-cd "${tmp_dir}"
+tmp_dir="/tmp/ws-${RANDOM}"
+mkdir -p "$tmp_dir"
+cd "$tmp_dir"
 
-# downloads last release
-last_tag=$(git ls-remote --tags https://github.com/caian-org/workstation | awk -F '/' '{print $3}' | tail -n 1)
-wget "https://github.com/caian-org/workstation/archive/${last_tag}.tar.gz"
+# ...
+case "$OSTYPE" in
+    linux-gnu)
+        preinstall_manjaro
+        run_playbook "manjaro"
+        ;;
 
-# installs yay (aur helper)
-git clone https://aur.archlinux.org/yay.git
-(
-    cd yay
-    makepkg -si --noconfirm
-)
-
-# extracts the release tarball
-tar xzf "${last_tag}.tar.gz"
-
-# runs the ansible playbook
-cd workstation-*
-make
+    darwin*)
+        preinstall_macos
+        run_playbook "macos"
+        ;;
+esac
